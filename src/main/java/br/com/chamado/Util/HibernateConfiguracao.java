2fa7150536ac406893fc35e4503f7dcea0cc47f0
CRC-TICWAN <CRC-TICWAN@CRC-TICWAN-PC>
2015-12-09 09:50:25 -0200

    Revert "model"
    This reverts commit 3a80d86ad3b6982f3a5a13eaea651f3364169a42.

---
+++ src/main/java/br/com/chamado/Util/HibernateConfiguracao.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.chamado.Util;
+
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.annotations.common.util.impl.Log;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+
+
+/**
+ *
+ * @author wandersonSantiago
+ */
+
+public class HibernateConfiguracao {
+  
+    private static SessionFactory sessionFactory;
+    
+    
+    public Session openSession()
+    {
+         
+	if(sessionFactory == null)
+	 {
+	   
+	     criaSessionFactory();   
+     	 }
+       return sessionFactory.openSession();
+    }
+    public void criaSessionFactory()
+    {
+         Configuration configuracao = new Configuration();
+	
+	 
+	 configuracao.addAnnotatedClass(Log.class);
+	 ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuracao.getProperties()).build();
+	 
+	 sessionFactory = configuracao.buildSessionFactory(serviceRegistry);
+	 
+	
+    }
+}
+
+
